fb268362ea1a9ba87c12f7187a070078250ac0ce
masontheplant <63219338+masontheplant@users.noreply.github.com>
2021-05-14 22:35:04 -0500

    stuck on how to make and document teams, preuming it is plural and more than 2

--- dice_roller.py
+++ dice_roller.py
@@ -2,6 +2,9 @@ import random
 dice_rolls = int(input('How many dice would you like to roll? '))
 dice_size = int(input('How many sides are the dice? '))
 dice_sum = 0
+player_amount = int(input('How many players or teams will play?'))
+for i in range(0, player_amount):
+ input('what is your team name?') # I'm not sure if this will work. Just two teams????
 for i in range(0, dice_rolls):
     roll = random.randint(1, dice_size)
     dice_sum += roll
